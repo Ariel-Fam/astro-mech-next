04734f0a5eb5f6eebc968761866ca5e0110a1a53
Ariel Wadyese <arielwadyese@gmail.com>
2025-05-09 22:12:58 -0500

    Most recent
    Added all planet images

--- app/aerthys/page.tsx
+++ app/aerthys/page.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Image from "next/image";
-
+import styles from "../styles/aerthys.module.css"
 
 export default function aerthys() {
 
     return(
 
+        <>
+        
+        
         <div>Aerthys</div>
+        
+        </>
+
 
     )
 
